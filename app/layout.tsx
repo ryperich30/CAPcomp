f51f060497236ccf4db1c02a7951edc2bbb54a2f
ryperich30 <ryanperich@gmail.com>
2025-09-28 13:58:31 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,11 @@
 import './globals.css'
-import Header from './components/Header' // if you don't have Header yet, you can remove this line
 
-export const metadata = {
-  title: 'CAPcomp',
-  description: 'Sports salary & stats',
-  // Tell browsers/socials exactly which files to use
-  icons: {
-    icon: '/favicon.png',            // you uploaded this to /public
-    // If you later add apple touch icons, add:
-    // apple: '/apple-touch-icon.png',
-  },
-  openGraph: {
-    title: 'CAPcomp',
-    description: 'Sports salary & stats',
-    images: ['/og.png'],             // you uploaded this to /public
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'CAPcomp',
-    description: 'Sports salary & stats',
-    images: ['/og.png'],
-  },
-}
+export const metadata = { title: 'CAPcomp', description: 'Sports salary & stats' }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0 }}>
-        {/* Remove <Header /> if you don't have it */}
-        <Header />
+      <body>
         {children}
       </body>
     </html>
